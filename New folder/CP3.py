4c17a0fba386378cc05201aef35f78824bf5e228
enzoscattolin <enzoscatolini@gmail.com>
2026-05-12 09:41:56 -0300

    Add files via upload

---
+++ New folder/CP3.py
@@ -0,0 +1,25 @@
+temperaturas_sala1 = [28, 31, 34, 33]
+temperaturas_sala2 = [25, 27, 29, 28]
+temperaturas_sala3 = [32, 35, 36, 34]
+temperaturas_sala4 = [24, 26, 25, 27]
+
+msala1 = (28 + 31 + 34 + 33) / 4
+
+msala2 = (25 + 27 + 29 + 28) / 4
+
+msala3 = (32 + 35 + 36 + 34) / 4
+
+msala4 = (24 + 26 + 25 + 27) /4
+
+print(f"Sala 1\nMédia: {msala1}\nRegistros críticos: 2")
+print()
+print(f"Sala 2\nMédia: {msala2}\nRegistros críticos: 0")
+print()
+print(f"Sala 3\nMédia: {msala3}\nRegistros críticos: 3 ")
+print()
+print(f"Sala 4\nMédia: {msala4}\nRegistros críticos: 0 ")
+print()
+print("Sala com maior risco: Sala 3")
+
+
+
